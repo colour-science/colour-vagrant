65bcb0a6324355c035a9eb6e014e97dd9abce7bc
Thomas Mansencal <thomas.mansencal@gmail.com>
2014-10-03 22:32:13 +0200

    Add "Latex" to package dependencies.

--- fabfile.py
+++ fabfile.py
@@ -50,6 +50,7 @@ BASH_PROFILE_FILE = os.path.join(HOME_DIRECTORY, '.bash_profile')
 
 REQUIRED_DEBIAN_PACKAGES = [
     'apache2',
+    'dvipng',
     'expect',
     'fontconfig',
     'git',
@@ -57,6 +58,8 @@ REQUIRED_DEBIAN_PACKAGES = [
     'php5',
     'libsm6',
     'libxrender-dev',
+    'texlive',
+    'texlive-latex-extra',
     'wget']
 
 SOFTWARES_URLS = {
